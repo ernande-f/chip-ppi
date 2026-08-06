dd53764723a3a369568bc35264a6c7b565ac9938
Ernande Facco <ernandefacco@outlook.com>
2026-08-06 09:37:23 -0300

    renomear pasta conflitante

---
+++ ldap/instrucoes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="https://www3.fw.iffarroupilha.edu.br/auth/index.php" method="POST">
+            <input type="text" value="CPF" name="user">
+            <input type="password" value="Senha" name="pass">
+            <input type="text" value="L ou S (LDAP ou SIGAA)" name="tipo">
+            <input type="submit">
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
